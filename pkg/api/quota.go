dcc2bea37668ade26357b466fff28faee875126b
gamab <gabi.mabs@gmail.com>
2021-09-23 14:14:57 +0200

    Forgot to save quota

--- pkg/api/quota.go
+++ pkg/api/quota.go
@@ -20,7 +20,7 @@ func (hs *HTTPServer) GetOrgQuotas(c *models.ReqContext) response.Response {
 
 func (hs *HTTPServer) getOrgQuotasHelper(c *models.ReqContext, orgID int64) response.Response {
 	hasAccess := accesscontrol.HasAccess(hs.AccessControl, c)
-	if !hasAccess(accesscontrol.NoReq, accesscontrol.EvalPermission(ActionOrgsRead, buildOrgIdScope(c.OrgId))) {
+	if !hasAccess(accesscontrol.NoReq, accesscontrol.EvalPermission(ActionOrgsRead, buildOrgsIdScope(c.OrgId))) {
 		return response.Error(403, "Access denied to org", nil)
 	}
 
