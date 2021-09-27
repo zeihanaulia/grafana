83f8aee621ffb3ae636befe3d48480ee13cadd8f
gamab <gabi.mabs@gmail.com>
2021-09-27 17:27:29 +0200

    Add missing tests

--- pkg/api/quota_test.go
+++ pkg/api/quota_test.go
@@ -1,29 +1,20 @@
 package api
 
 import (
-	"context"
-	"io"
+	"fmt"
 	"net/http"
-	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
-	macaron "gopkg.in/macaron.v1"
 
-	"github.com/grafana/grafana/pkg/api/routing"
-	"github.com/grafana/grafana/pkg/bus"
-	"github.com/grafana/grafana/pkg/infra/log"
 	"github.com/grafana/grafana/pkg/models"
 	"github.com/grafana/grafana/pkg/services/accesscontrol"
-	accesscontrolmock "github.com/grafana/grafana/pkg/services/accesscontrol/mock"
-	"github.com/grafana/grafana/pkg/services/quota"
-	"github.com/grafana/grafana/pkg/services/sqlstore"
 	"github.com/grafana/grafana/pkg/setting"
 )
 
-var getCurrentOrgUrl = "/api/org/"
 var getCurrentOrgQuotasUrl = "/api/org/quotas"
+var getOrgsQuotasUrl = "/api/orgs/%v/quotas"
 
 var testOrgQuota = setting.OrgQuota{
 	User:       10,
@@ -33,107 +24,8 @@ var testOrgQuota = setting.OrgQuota{
 	AlertRule:  10,
 }
 
-func setAccessControlPermissions(acmock *accesscontrolmock.Mock, perms []*accesscontrol.Permission) {
-	acmock.GetUserPermissionsFunc = func(_ context.Context, _ *models.SignedInUser) ([]*accesscontrol.Permission, error) {
-		return perms, nil
-	}
-}
-
-func setupHTTPServer(t *testing.T, enableAccessControl bool, signedInUser *models.SignedInUser) (*macaron.Macaron, *HTTPServer, *accesscontrolmock.Mock) {
-	t.Helper()
-
-	// Use an accesscontrol mock
-	acmock := accesscontrolmock.New()
-	if !enableAccessControl {
-		acmock = acmock.WithDisabled()
-	}
-
-	// Use a new conf
-	cfg := setting.NewCfg()
-	cfg.FeatureToggles = make(map[string]bool)
-	cfg.FeatureToggles["accesscontrol"] = enableAccessControl
-
-	// Use a test DB
-	db := sqlstore.InitTestDB(t)
-	db.Cfg = cfg
-
-	// Create minimal HTTP Server
-	hs := &HTTPServer{
-		Cfg:           cfg,
-		Bus:           bus.GetBus(),
-		Live:          newTestLive(t),
-		QuotaService:  &quota.QuotaService{Cfg: cfg},
-		RouteRegister: routing.NewRouteRegister(),
-		AccessControl: acmock,
-		SQLStore:      db,
-	}
-
-	// Instantiate a new Server
-	m := macaron.New()
-
-	// Pretend middleware to sign the user in
-	if signedInUser != nil {
-		m.Use(func(c *macaron.Context) {
-			ctx := &models.ReqContext{
-				Context:      c,
-				IsSignedIn:   true,
-				SignedInUser: signedInUser,
-				Logger:       log.New("api-test"),
-			}
-			c.Map(ctx)
-		})
-	}
-
-	// Register all routes
-	hs.registerRoutes()
-	hs.RouteRegister.Register(m.Router)
-
-	return m, hs, acmock
-}
-
-func callAPI(server *macaron.Macaron, method, path string, body io.Reader, t *testing.T) *httptest.ResponseRecorder {
-	req, err := http.NewRequest(method, path, body)
-	require.NoError(t, err)
-	req.Header.Set("Content-Type", "application/json")
-	recorder := httptest.NewRecorder()
-	server.ServeHTTP(recorder, req)
-	return recorder
-}
-
-func TestAPIEndpoint_GetCurrentOrg_LegacyAccessControl(t *testing.T) {
-	testuser := &models.SignedInUser{UserId: testUserID, OrgId: testOrgID, OrgRole: models.ROLE_VIEWER, Login: testUserLogin}
-	server, hs, _ := setupHTTPServer(t, false, testuser)
-
-	_, err := hs.SQLStore.CreateOrgWithMember("TestOrg", testUserID)
-	require.NoError(t, err)
-
-	t.Run("Viewer can view CurrentOrg", func(t *testing.T) {
-		response := callAPI(server, http.MethodGet, getCurrentOrgUrl, nil, t)
-		assert.Equal(t, http.StatusOK, response.Code)
-	})
-}
-
-func TestAPIEndpoint_GetCurrentOrg_AccessControl(t *testing.T) {
-	testuser := &models.SignedInUser{UserId: testUserID, OrgId: testOrgID, OrgRole: models.ROLE_VIEWER, Login: testUserLogin}
-	server, hs, acmock := setupHTTPServer(t, true, testuser)
-
-	_, err := hs.SQLStore.CreateOrgWithMember("TestOrg", testUserID)
-	require.NoError(t, err)
-
-	t.Run("AccessControl allows viewing CurrentOrg with correct permissions", func(t *testing.T) {
-		setAccessControlPermissions(acmock, []*accesscontrol.Permission{{Action: ActionOrgsRead, Scope: ScopeOrgsAll}})
-		response := callAPI(server, http.MethodGet, getCurrentOrgUrl, nil, t)
-		assert.Equal(t, http.StatusOK, response.Code)
-	})
-	t.Run("AccessControl prevents viewing CurrentOrg with incorrect permissions", func(t *testing.T) {
-		setAccessControlPermissions(acmock, []*accesscontrol.Permission{{Action: "orgs:invalid"}})
-		response := callAPI(server, http.MethodGet, getCurrentOrgUrl, nil, t)
-		assert.Equal(t, http.StatusForbidden, response.Code)
-	})
-}
-
 func TestAPIEndpoint_GetCurrentOrgQuotas_LegacyAccessControl(t *testing.T) {
-	testuser := &models.SignedInUser{UserId: testUserID, OrgId: testOrgID, OrgRole: models.ROLE_VIEWER, Login: testUserLogin}
+	testuser := &models.SignedInUser{UserId: testUserID, OrgId: 1, OrgRole: models.ROLE_VIEWER, Login: testUserLogin}
 	server, hs, _ := setupHTTPServer(t, false, testuser)
 
 	hs.Cfg.Quota.Enabled = true
@@ -151,7 +43,7 @@ func TestAPIEndpoint_GetCurrentOrgQuotas_LegacyAccessControl(t *testing.T) {
 }
 
 func TestAPIEndpoint_GetCurrentOrgQuotas_AccessControl(t *testing.T) {
-	testuser := &models.SignedInUser{UserId: testUserID, OrgId: testOrgID, OrgRole: models.ROLE_VIEWER, Login: testUserLogin}
+	testuser := &models.SignedInUser{UserId: testUserID, OrgId: 1, OrgRole: models.ROLE_VIEWER, Login: testUserLogin}
 	server, hs, acmock := setupHTTPServer(t, true, testuser)
 
 	hs.Cfg.Quota.Enabled = true
@@ -167,9 +59,67 @@ func TestAPIEndpoint_GetCurrentOrgQuotas_AccessControl(t *testing.T) {
 		response := callAPI(server, http.MethodGet, getCurrentOrgQuotasUrl, nil, t)
 		assert.Equal(t, http.StatusOK, response.Code)
 	})
+	t.Run("AccessControl allows viewing CurrentOrgQuotas with exact permissions", func(t *testing.T) {
+		setAccessControlPermissions(acmock, []*accesscontrol.Permission{{Action: ActionOrgsRead, Scope: "orgs:1"}})
+		response := callAPI(server, http.MethodGet, getCurrentOrgQuotasUrl, nil, t)
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
 	t.Run("AccessControl prevents viewing CurrentOrgQuotas with incorrect permissions", func(t *testing.T) {
 		setAccessControlPermissions(acmock, []*accesscontrol.Permission{{Action: "orgs:invalid"}})
 		response := callAPI(server, http.MethodGet, getCurrentOrgQuotasUrl, nil, t)
+		assert.Equal(t, http.StatusForbidden, response.Code)
+	})
+}
+
+func TestAPIEndpoint_GetOrgQuotas_LegacyAccessControl(t *testing.T) {
+	testuser := &models.SignedInUser{UserId: testUserID, OrgId: 1, OrgRole: models.ROLE_ADMIN, Login: testUserLogin, IsGrafanaAdmin: true}
+	server, hs, _ := setupHTTPServer(t, false, testuser)
+
+	hs.Cfg.Quota.Enabled = true
+	hs.Cfg.Quota.Org = &testOrgQuota
+	// Required while sqlstore quota.go relies on setting global variables
+	setting.Quota = hs.Cfg.Quota
+
+	// Create two orgs, to fetch another one than the logged in one
+	_, err := hs.SQLStore.CreateOrgWithMember("TestOrg", testUserID)
+	require.NoError(t, err)
+	_, err = hs.SQLStore.CreateOrgWithMember("TestOrg2", testUserID)
+	require.NoError(t, err)
+
+	t.Run("Admin can view another org quotas", func(t *testing.T) {
+		response := callAPI(server, http.MethodGet, fmt.Sprintf(getOrgsQuotasUrl, 2), nil, t)
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
+}
+
+func TestAPIEndpoint_GetOrgQuotas_AccessControl(t *testing.T) {
+	testuser := &models.SignedInUser{UserId: testUserID, OrgId: 1, OrgRole: models.ROLE_VIEWER, Login: testUserLogin}
+	server, hs, acmock := setupHTTPServer(t, true, testuser)
+
+	hs.Cfg.Quota.Enabled = true
+	hs.Cfg.Quota.Org = &testOrgQuota
+	// Required while sqlstore quota.go relies on setting global variables
+	setting.Quota = hs.Cfg.Quota
+
+	// Create two orgs, to fetch another one than the logged in one
+	_, err := hs.SQLStore.CreateOrgWithMember("TestOrg", testUserID)
+	require.NoError(t, err)
+	_, err = hs.SQLStore.CreateOrgWithMember("TestOrg2", testUserID)
+	require.NoError(t, err)
+
+	t.Run("AccessControl allows viewing another org quotas with correct permissions", func(t *testing.T) {
+		setAccessControlPermissions(acmock, []*accesscontrol.Permission{{Action: ActionOrgsRead, Scope: ScopeOrgsAll}})
+		response := callAPI(server, http.MethodGet, fmt.Sprintf(getOrgsQuotasUrl, 2), nil, t)
+		assert.Equal(t, http.StatusOK, response.Code)
+	})
+	t.Run("AccessControl prevents viewing another org quotas with too narrow permissions", func(t *testing.T) {
+		setAccessControlPermissions(acmock, []*accesscontrol.Permission{{Action: ActionOrgsRead, Scope: "orgs:1"}})
+		response := callAPI(server, http.MethodGet, fmt.Sprintf(getOrgsQuotasUrl, 2), nil, t)
+		assert.Equal(t, http.StatusForbidden, response.Code)
+	})
+	t.Run("AccessControl prevents viewing another org quotas with incorrect permissions", func(t *testing.T) {
+		setAccessControlPermissions(acmock, []*accesscontrol.Permission{{Action: "orgs:invalid"}})
+		response := callAPI(server, http.MethodGet, fmt.Sprintf(getOrgsQuotasUrl, 2), nil, t)
 		assert.Equal(t, http.StatusForbidden, response.Code)
 	})
 }
